53ab35eb87758c84a31d9d6f3397675296ac0457
Gui Heurich <guilherme.heurich@protonmail.com>
2022-11-28 11:30:26 +0000

    Extract Backstage Passes

--- ruby/gilded_rose.rb
+++ ruby/gilded_rose.rb
@@ -4,34 +4,20 @@ class GildedRose
     @items = items
   end
 
-  def update_general_item(item)
-    item.quality -= 1 unless item.quality.zero?
-    item.quality -= 1 if item.sell_in.negative?
-    item.sell_in -= 1
-  end
-
   def update_quality()
     @items.each do |item|
       if item.name == "general item"
         return update_general_item(item)
       end
 
+      if item.name == "Backstage passes to a TAFKAL80ETC concert"
+        return update_backstage_passes(item)
+      end
+
       if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert"
       else
         if item.quality < 50
           item.quality = item.quality + 1
-          if item.name == "Backstage passes to a TAFKAL80ETC concert"
-            if item.sell_in < 11
-              if item.quality < 50
-                item.quality = item.quality + 1
-              end
-            end
-            if item.sell_in < 6
-              if item.quality < 50
-                item.quality = item.quality + 1
-              end
-            end
-          end
         end
       end
       if item.name != "Sulfuras, Hand of Ragnaros"
@@ -49,6 +35,21 @@ class GildedRose
           end
         end
       end
+    end
+  end
+
+  def update_general_item(item)
+    item.quality -= 1 unless item.quality.zero?
+    item.quality -= 1 if item.sell_in.negative?
+    item.sell_in -= 1
+  end
+
+  def update_backstage_passes(item)
+    return item.quality = 0 if item.sell_in <= 0
+    if item.sell_in <= 5
+      item.quality += 3
+    elsif item.sell_in <= 10
+      item.quality += 2
     end
   end
 end
